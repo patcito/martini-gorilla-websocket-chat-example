06d43c2c99f0203a538650fd9d536369bd56fd92
Patrick Aljord <patcito@gmail.com>
2013-12-26 12:07:54 +0000

    make use of mutex for concurency all credits go to @deckarep
    see for more info https://gist.github.com/deckarep/8125909

--- server.go
+++ server.go
@@ -1,19 +1,44 @@
 package main
-
+ 
 import (
-  "github.com/gorilla/websocket" 
   "github.com/codegangsta/martini"
+  "github.com/gorilla/websocket"
+  "log"
   "net"
   "net/http"
-  "log"
+  "sync"
 )
+ 
+/*
+  Go is a great langauge but you have to be aware of what happens when you are mutating state from multiple goroutines.
+  Everytime a web request comes in, in your /sock handler down below Go will create a new goroutine to service that
+  request, this means, you could potentially have a lot of new gourtines concurrently running that are modifiying the
+  ActiveClients map defined below this text.  By design, maps are not concurrent-safe, meaning that if they are being
+  modified by multiple goroutines concurrently, they can easily get into an invalid state.
+
+  If all the /sock handler was doing, was only reading to but not writing the ActiveClients, then you would be fine.
+  But in this script you are also writing to the ActiveClients therefore you must synchronize access to map.
+
+  You can do that one of two ways: Using channels, or using mutexes.  These topics are fairly big topics on their own
+  but here is how you can use a mutex to make sure that are safely mutating the state of the map.
+
+  See this page for further reading: http://blog.golang.org/go-maps-in-action
+
+*/
 
 var ActiveClients = make(map[ClientConn]int)
+var ActiveClientsRWMutex sync.RWMutex
 
 type ClientConn struct {
-   websocket *websocket.Conn
-   clientIP  net.Addr
- }
+  websocket *websocket.Conn
+  clientIP  net.Addr
+}
+
+func addClient(cc ClientConn) {
+  ActiveClientsRWMutex.Lock()
+  ActiveClients[cc] = 0
+  ActiveClientsRWMutex.Unlock()
+}
 
 func main() {
   m := martini.Classic()
@@ -36,7 +61,7 @@ func main() {
     }
     </script></body></html>`
   })
-  m.Get("/sock", func (w http.ResponseWriter, r *http.Request) {
+  m.Get("/sock", func(w http.ResponseWriter, r *http.Request) {
     log.Println(ActiveClients)
     ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
     if _, ok := err.(websocket.HandshakeError); ok {
@@ -48,8 +73,8 @@ func main() {
     }
     client := ws.RemoteAddr()
     sockCli := ClientConn{ws, client}
-    ActiveClients[sockCli] = 0 
-
+    addClient(sockCli)
+ 
     for {
       messageType, p, err := ws.ReadMessage()
       if err != nil {
